9f93980194aff5abc4cba1b7a05edcda4b4f4d17
christian-fr <karl.wilhelm@posteo.de>
2020-08-02 05:34:33 +0200

    incremented version

--- run_Qml_Reader.py
+++ run_Qml_Reader.py
@@ -1,7 +1,7 @@
 __author__ = "Christian Friedrich"
 __maintainer__ = "Christian Friedrich"
 __license__ = "GPL v3"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __status__ = "Prototype"
 __name__ = "QmlReader_GUI"
 # last edited 2020-04-01
