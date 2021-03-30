a4c71e9967aa4b732189977e6a54885d765b4335
christian-fr <karl.wilhelm@posteo.de>
2021-03-30 17:23:47 +0200

    questionnaire.py type hints added; mainInterface.py version incremented, conditions added to details string; main.py version incremented;

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 __author__ = "Christian Friedrich"
 __maintainer__ = "Christian Friedrich"
 __license__ = "MIT"
-__version__ = "1.0.11"
+__version__ = "1.0.12"
 __status__ = "Prototype"
 __name__ = "QmlReader_GUI"
 # last edited 2020-04-01
