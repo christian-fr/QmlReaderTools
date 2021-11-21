2914dcabf48fb952829eecbb86a98df160fb94c7
christian-fr <friedrich@dzhw.eu>
2021-11-21 08:51:24 +0100

    added setup.py and requirements.txt updated LICENSE.md

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License
 
-_Copyright (c) 2020 Christian Friedrich_
+_Copyright (c) 2021 Christian Friedrich_
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='QmlReaderTools',
+    version='1.0.14',
+    packages=['gui', 'tests', 'codebook', 'pdfOutput', 'qmlReader', 'screenshotter', 'advancedFlowchart'],
+    url='https://github.com/christian-fr/QmlReaderTools',
+    license='MIT',
+    author='Christian Friedrich',
+    author_email='mail-python@chr-fr.net',
+    description='QmlReaderTools - Toolbox for processing and analyzing QML files'
+)
