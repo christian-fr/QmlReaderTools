cdd9c1adc8a5bdb839802bb6e944eed9602449eb
christian-fr <friedrich@dzhw.eu>
2022-02-08 12:50:30 +0100

    updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 __author__ = "Christian Friedrich"
 __maintainer__ = "Christian Friedrich"
 __license__ = "MIT"
-__version__ = "1.0.14"
+__version__ = "1.0.15"
 __status__ = "Prototype"
 __name__ = "QmlReader_GUI"
 
